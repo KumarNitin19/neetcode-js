2222daf61c21f9a3ec297b1123cb03b30419a6e2
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2026-02-21 22:06:42 +0530

    feat: 22 - generate parenthesis solution

---
+++ backtracking/22-generate-parenthesis.js
@@ -0,0 +1,38 @@
+/**
+ * 22. Generate Parentheses
+ * https://leetcode.com/problems/generate-parentheses/
+ *
+ * Time Complexity: O(4^n / √n)
+ * Space Complexity: O(n)
+ */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    const res = [];
+    const stack = [];
+
+    function backtrack(open, close) {
+        if (open === n && close === n) {
+            res.push(stack.join(""));
+            return;
+        }
+
+        if (open < n) {
+            stack.push("(");
+            backtrack(open + 1, close);
+            stack.pop();
+        }
+
+        if (close < open) {
+            stack.push(")");
+            backtrack(open, close + 1);
+            stack.pop();
+        }
+    }
+
+    backtrack(0, 0);
+    return res;
+};
