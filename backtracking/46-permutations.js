caf874446e513efbfed16b3759313e7850e9372b
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2026-02-21 22:05:32 +0530

    fix: improved 46 - permutations solution

--- backtracking/46-permutations.js
+++ backtracking/46-permutations.js
@@ -7,31 +7,48 @@
  */
 
 /**
+ * Recursive
  * @param {number[]} nums
  * @return {number[][]}
  */
 var permute = function (nums) {
+    if (nums.length === 0) {
+        return [[]];
+    }
+
+    const permu = permute(nums.slice(1));
     const res = [];
-    const used = new Array(nums.length).fill(false);
-    const curr = [];
 
-    function dfs() {
-        if (curr.length === nums.length) {
-            res.push([...curr]);
-            return;
-        }
-
-        for (let i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-
-            used[i] = true;
-            curr.push(nums[i]);
-            dfs();
-            curr.pop();
-            used[i] = false;
+    for (const p of permu) {
+        for (let i = 0; i <= p.length; i++) {
+            const permu_copy = [...p];
+            permu_copy.splice(i, 0, nums[0]);
+            res.push(permu_copy);
         }
     }
 
-    dfs();
     return res;
+};
+
+/**
+ * Iterative
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    let permu = [[]];
+
+    for (const n of nums) {
+        const new_permu = [];
+        for (const p of permu) {
+            for (let i = 0; i <= p.length; i++) {
+                const p_copy = [...p];
+                p_copy.splice(i, 0, n);
+                new_permu.push(p_copy);
+            }
+        }
+        permu = new_permu;
+    }
+
+    return permu;
 };
